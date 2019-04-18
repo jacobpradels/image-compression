651b84716bdcbbdd3e3e2ebee5f27ab7aebd25b0
jacobpradels <pradelsjacob@gmail.com>
2019-04-17 19:55:06 -0700

    Alpha Version 0.01 Distribution

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from PIL import Image
+from path import pathtolist
+import tkinter
 
-def compressImg(img):
-    foo = Image.open("sunflower.jpg")
-    print(foo.size)
-    print(foo.size[0])
-    print(foo.size[1])
-    foo = foo.resize((int(foo.size[0]/10),int(foo.size[1]/10)),Image.ANTIALIAS)
-    foo.save("image_scaled.jpg",quality=95)
+
+scaleFactor = 1
+def compressImg(img,dir):
+    foo = Image.open(str(dir) + "\\" + img)
+    # print(foo.size,foo.size)
+    foo = foo.resize((int(foo.size[0]/scaleFactor),int(foo.size[1]/scaleFactor)),Image.ANTIALIAS)
+    foo.save(dir + "\\c-" + img,quality=10)
+
+
+top = tkinter.Tk()
+top.title("Image compression")
+top.geometry("500x500")
+
+
+w = tkinter.Label(top, text="Input the file path in this text box:")
+w.pack()
+
+e = tkinter.Entry(top)
+e.pack()
+
+def outputit():
+    string = e.get()
+    dir = str(string)
+    path = pathtolist(dir)
+    # print(path)
+    # print(dir)
+    for file in path:
+        compressImg(file,dir)
+        # print(type(file))
+    
+
+b = tkinter.Button(top, text="OK", command=outputit)
+b.pack()
+tkinter.mainloop()
+
+    
